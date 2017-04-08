b1d2e6999dc6f2846ea32866dcca259170fc121e
rojasa19 <rojasadrian.e@gmail.com>
2017-04-08 11:32:16 -0300

    Se agregan comentarios

--- ejercicio5/main.cpp
+++ ejercicio5/main.cpp
@@ -4,22 +4,23 @@ using namespace std;
 
 /** \brief
  * Dado un nuemero y entero de 8 digitos con formato aaaammdd descompongalo en sus elementos logicos e imprimalo por pantalla
- * \param
- * \param
+ * \param Int fecha
  * \return
  *
  */
 int main()
 {
-    long fecha;
-    int dia, mes, anio;
+    //Declaro variables
+    int fecha, dia, mes, anio;
+    //Solicito dato
     cout << "Ingrese un valor con formato aaaammdd" << endl;
+    //Asigno dato a la variable
     cin >> fecha;
-
+    //Calculo los datos
     anio = fecha / 10000;
     mes = (fecha / 100)%100;
     dia = fecha % 100;
-
+    //Imprimo los datos
     cout << "Dia: " << dia << endl;
     cout << "Mes: " << mes << endl;
     cout << "Anio: " << anio << endl;
